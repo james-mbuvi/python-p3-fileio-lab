530bddc0a8c81fed4604c9df5b8ff6c2c1ee96fd
james-mbuvi <james.mbuvi@student.moringaschool.com>
2024-03-06 17:17:18 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,30 @@
-def write_file(file_name, file_content):
-    pass
 
-def append_file(file_name, append_content):
+def write_file(file_name, file_content):
+   with open(str(file_name) + '.txt', 'w') as file_name:
+       file_name.write(file_content)
+   
+   
+   pass
+
+
+
+def append_file(file_name, file_content):
+    with open(str(file_name) + '.txt', 'a') as file_name:
+        file_name.write(file_content)
+
+
     pass
 
 def read_file(file_name):
+    with open(str(file_name) + '.txt', 'r') as file:
+        return file.read()
     pass
+
+
+
+
+
+
+
+
+
